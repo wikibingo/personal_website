0002623c77b60952ca72a89c50b9518a1f721a44
Wenli Xu <wikibingo@aim.com>
2023-03-14 11:07:02 -0700

    Add loop to check if user input in courseList.

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,20 @@ const courseList = [
       name: "Database Systems"
     }
   ];
+
+let input;
+do{
+    input = prompt("Enter a course code: ");
+} while(isNaN(input) === true || input.length !==4);
+let courseFound = false;
+for (let i of courseList){
+    if (i.code.includes(input)){
+        console.log(`Yes I am taking the course:  ${i.code} - ${i.name}`);
+        courseFound = true;
+        break;
+    }    
+}
+if (!courseFound) {  
+    courseList.push({ code: input, name: null });
+    console.log(`Added course ${input} successful`);
+}
