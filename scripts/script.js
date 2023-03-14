b05dc5717dcefadbe530232eb4c4d0b110705398
Wenli Xu <wikibingo@aim.com>
2023-03-14 09:11:44 -0700

    Define three objects in courselist array

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const courseList = [
+    {
+      code: "ACIT 1620",
+      name: "Web Fundamental Technologies"
+    },
+    {
+      code: "ACIT 1515",
+      name: "Scripting for IT"
+    },
+    {
+      code: "ACIT 1420",
+      name: "Database Systems"
+    }
+  ];
